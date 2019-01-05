663bf4c50b2f730aadb8f968d90885860eb3e618
random25umezawa <bday7125@gmail.com>
2019-01-05 23:42:07 +0900

    たぶん回転なども正しく反映された。

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const Vox = require("./voxtool.js")
 
 let vx = new Vox(fs.readFileSync("./reftestvox.vox"));
 //console.log(vx.chunk);
-console.log(JSON.stringify(vx.chunk,null,"\t"))
+//console.log(JSON.stringify(vx.chunk,null,"\t"))
 let merged = vx.getModel(vx.nodes[0]);
 let y_tiles = {};
-for(let block of merged) {
+for(let block of merged[2]) {
 	if(!y_tiles[block[1]]) y_tiles[block[1]] = [];
 	y_tiles[block[1]].push(block);
 }
