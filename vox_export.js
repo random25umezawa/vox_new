ac70258d917a5031c7d5b810813516706326b0a0
random25umezawa <bday7125@gmail.com>
2019-01-06 14:40:21 +0900

    回転画像がわりといい感じにできた。

--- vox_export.js
+++ vox_export.js
@@ -8,21 +8,47 @@ class VoxExport {
 	}
 
 	async exportDanmens(path) {
-		let ys = [...Array(this.size[1]).keys()];
-		let layers = [...Array(this.size[1]).keys()].map(_=>[]);
+		let zs = [...Array(this.size[2]).keys()];
+		let layers = zs.map(_=>[]);
 		for(let block of this.blocks) {
-			layers[block[1]].push(block);
+			layers[block[2]].push(block);
 		}
-		let danmen_images = await Promise.all(ys.map(y=>this.createDanmen(y,layers[y])));
-		for(let y of ys) {
-			danmen_images[y].write(`${y}_${path}`);
+		let danmen_images = await Promise.all(zs.map(z=>this.createDanmen(z,layers[z])));
+		/*
+		for(let i = 1; i < danmen_images.length; i++) {
+			danmen_images[0].composite(danmen_images[i],0,i);
+		}
+		danmen_images[0].write(`${path}`);
+		*/
+
+		let splits = 120;
+		let angles = [...Array(splits).keys()].map(num=>360*num/splits);
+		let rotate_images = await Promise.all(angles.map(angle=>this.createRotateImage(angle,danmen_images)));
+		for(let i = 0; i < rotate_images.length; i++) {
+			rotate_images[i].resize(rotate_images[i].bitmap.width*4,rotate_images[i].bitmap.height*4,Jimp.RESIZE_NEAREST_NEIGHBOR).write(`rotate/${i}_${path}`);
 		}
 	}
 
-	async createDanmen(y,layer) {
-		let img = await this.createImageAsync(this.size[0],this.size[2]);
+	async createDanmen(z,layer) {
+		let path_long = Math.max(this.size[0],this.size[1]);
+		let img = await this.createImageAsync(path_long*2,path_long*2);
 		for(let block of layer) {
-			img.setPixelColor((this.palette[block[3]]>>>0),block[0],block[2]);
+			img.setPixelColor((this.palette[block[3]]>>>0),block[0]+path_long/2+(path_long-this.size[0])/2,block[1]+path_long/2+(path_long-this.size[1])/2);
+		}
+		return img.flip(false,true);
+	}
+
+	async createRotateImage(angle,danmen_images) {
+		let path_long = Math.max(this.size[0],this.size[1]);
+		let img = await this.createImageAsync(path_long*2*2,path_long*2+this.size[2]*2-1);
+		for(let i = 0; i < danmen_images.length; i++) {
+			let danmen_image = danmen_images[i].clone();
+			danmen_image.resize(danmen_image.bitmap.width*2,danmen_image.bitmap.height*2,Jimp.RESIZE_NEAREST_NEIGHBOR);
+			danmen_image.rotate(angle,false);
+			danmen_image.resize(danmen_image.bitmap.width,danmen_image.bitmap.height/2,Jimp.RESIZE_NEAREST_NEIGHBOR);
+			for(let j = 0; j < 2; j++) {
+				img.composite(danmen_image,0,danmen_images.length*2-i*2-j-1);
+			}
 		}
 		return img;
 	}
